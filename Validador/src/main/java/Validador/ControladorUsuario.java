61867fd9024339d4c12c9cc8ad090336e4a43db6
FernandoHerbas <feer15.herbas@gmail.com>
2020-05-01 18:52:02 -0300

    Validador semiterminado Agrego ultima 2 funciones para el validador y falta chequear las librerias de importaancion de archivo

--- Validador/src/main/java/Validador/ControladorUsuario.java
+++ Validador/src/main/java/Validador/ControladorUsuario.java
@@ -6,20 +6,34 @@ import java.io.IOException;
 
 
 public class ControladorUsuario {
+    static String rutaArchivo10KPeoresContras = "\\src\\main\\resources\\10k-worst-passwords.txt";
     static int LONGITUDMINIMA = 8;
-    public enum EstadoContrasenia{CORTA, MUYCOMUN, INSUFICIENTE, VALIDA}
+    public enum EstadoContrasenia{CORTA, MUYCOMUN, INVALIDA, VALIDA}
 
     public EstadoContrasenia validarConstrasenia(String password,String user,String mail){
 
         if(longitudCorta(password))
-            return CORTA;
+            return EstadoContrasenia.CORTA;
         if(estaEnElTopDiezK(password))
-            return MUYCOMUN;
+            return EstadoContrasenia.MUYCOMUN;
+        if(esIgualAMailOUsuario(password,mail,user))
+            return EstadoContrasenia.INVALIDA;
+        return EstadoContrasenia.VALIDA;
     }
     protected boolean longitudCorta(String password) {
         return password.length() < LONGITUDMINIMA;
     }
-    protected boolean estaEnElTopDiezK(String password){
-        import
+    protected boolean estaEnElTopDiezK(String miPassword){
+        String unPassword;
+        FileReader archivo = new FileReader(rutaArchivo10KPeoresContras); //crea la ruta al archivo
+        BufferedReader entrada = new BufferedReader(archivo);             //lee la entrada de texto
+        while((unPassword = entrada.readLine()) != null){ //Vamos leyendo una linea del archivo. Verificar como funciona readLine()
+            if(miPassword == unPassword)
+                return true;
+        }
+        return false;
+    }
+    protected boolean esIgualAMailOUsuario(String password,String mail, String user){
+        return false;
     }
 }
