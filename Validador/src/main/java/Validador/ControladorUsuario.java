3be20b55433c09b8df3be7b4417c370ef8df77ce
FernandoHerbas <feer15.herbas@gmail.com>
2020-05-01 19:21:54 -0300

    Ultimos detalles, falta test Agrego lo que faltaba de exception al lado de las funciones, y agrego clase en diagrama ControlUsuario

--- Validador/src/main/java/Validador/ControladorUsuario.java
+++ Validador/src/main/java/Validador/ControladorUsuario.java
@@ -1,39 +1,38 @@
 package Validador;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class ControladorUsuario {
-    static String rutaArchivo10KPeoresContras = "\\src\\main\\resources\\10k-worst-passwords.txt";
-    static int LONGITUDMINIMA = 8;
+    private static String rutaArchivo10KPeoresContras = "\\src\\main\\resources\\10k-worst-passwords.txt";
+    private static int LONGITUDMINIMA = 8;
     public enum EstadoContrasenia{CORTA, MUYCOMUN, INVALIDA, VALIDA}
 
-    public EstadoContrasenia validarConstrasenia(String password,String user,String mail){
+    public EstadoContrasenia validarConstrasenia(String contrasenia,String user,String mail) throws IOException{
 
-        if(longitudCorta(password))
+        if(longitudCorta(contrasenia))
             return EstadoContrasenia.CORTA;
-        if(estaEnElTopDiezK(password))
+        if(estaEnElTopDiezK(contrasenia))
             return EstadoContrasenia.MUYCOMUN;
-        if(esIgualAMailOUsuario(password,mail,user))
+        if(esIgualAMailOUsuario(contrasenia,mail,user))
             return EstadoContrasenia.INVALIDA;
         return EstadoContrasenia.VALIDA;
     }
-    protected boolean longitudCorta(String password) {
-        return password.length() < LONGITUDMINIMA;
+    protected boolean longitudCorta(String contrasenia) {
+        return contrasenia.length() < LONGITUDMINIMA;
     }
-    protected boolean estaEnElTopDiezK(String miPassword){
-        String unPassword;
+    protected boolean estaEnElTopDiezK(String miContrasenia) throws IOException {
+        String unaContrasenia;
         FileReader archivo = new FileReader(rutaArchivo10KPeoresContras); //crea la ruta al archivo
         BufferedReader entrada = new BufferedReader(archivo);             //lee la entrada de texto
-        while((unPassword = entrada.readLine()) != null){ //Vamos leyendo una linea del archivo. Verificar como funciona readLine()
-            if(miPassword == unPassword)
+        while((unaContrasenia = entrada.readLine()) != null){ //Vamos leyendo una linea del archivo. Verificar como funciona readLine()
+            if(miContrasenia == unaContrasenia)
                 return true;
         }
         return false;
     }
-    protected boolean esIgualAMailOUsuario(String password,String mail, String user){
+    protected boolean esIgualAMailOUsuario(String contrasenia,String user, String mail){
         return false;
     }
 }
