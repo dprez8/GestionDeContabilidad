7c78d98bc30744893097bd658fc9b286c15c6d14
lait-utn <ulaitml@gmail.com>
2020-05-03 20:39:45 -0300

    Mejorado Validador de Contraseña
    Se agregaron tests al validador de contraseña, se arreglo un error con el chequeo de las contraseñas en la validacion contra el archivo de las 10K contraseñas.
    
    Co-authored-by: Otros Participantes

--- Validador/src/main/java/Validador/ControladorUsuario.java
+++ Validador/src/main/java/Validador/ControladorUsuario.java
@@ -3,36 +3,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class ControladorUsuario {
-    private static String rutaArchivo10KPeoresContras = "\\src\\main\\resources\\10k-worst-passwords.txt";
+    private static String rutaArchivo10KPeoresContras = "src/main/resources/10k-worst-passwords.txt";
     private static int LONGITUDMINIMA = 8;
-    public enum EstadoContrasenia{CORTA, MUYCOMUN, INVALIDA, VALIDA}
+    static public enum EstadoContrasenia{CORTA, MUYCOMUN, INVALIDA, VALIDA}
 
-    public EstadoContrasenia validarConstrasenia(String contrasenia,String user,String mail) throws IOException{
+    /**
+     *
+     * @param contrasenia
+     * @param user
+     * @param mail
+     * @return
+     * @throws IOException
+     */
+    static public EstadoContrasenia validarConstrasenia(String contrasenia, String user, String mail) throws IOException{
 
         if(longitudCorta(contrasenia))
             return EstadoContrasenia.CORTA;
         if(estaEnElTopDiezK(contrasenia))
             return EstadoContrasenia.MUYCOMUN;
-        if(esIgualAMailOUsuario(contrasenia,mail,user))
+        if(esIgualAMailOUsuario(contrasenia, mail, user))
             return EstadoContrasenia.INVALIDA;
         return EstadoContrasenia.VALIDA;
     }
-    protected boolean longitudCorta(String contrasenia) {
+    static protected boolean longitudCorta(String contrasenia) {
         return contrasenia.length() < LONGITUDMINIMA;
     }
-    protected boolean estaEnElTopDiezK(String miContrasenia) throws IOException {
+    static protected boolean estaEnElTopDiezK(String miContrasenia) throws IOException {
         String unaContrasenia;
         FileReader archivo = new FileReader(rutaArchivo10KPeoresContras); //crea la ruta al archivo
         BufferedReader entrada = new BufferedReader(archivo);             //lee la entrada de texto
-        while((unaContrasenia = entrada.readLine()) != null){ //Vamos leyendo una linea del archivo. Verificar como funciona readLine()
-            if(miContrasenia == unaContrasenia)
+        while((unaContrasenia = entrada.readLine()) != null) { //Vamos leyendo una linea del archivo. Verificar como funciona readLine()
+            if(miContrasenia.equals(unaContrasenia))
                 return true;
         }
         return false;
     }
-    protected boolean esIgualAMailOUsuario(String contrasenia,String user, String mail){
+    static protected boolean esIgualAMailOUsuario(String contrasenia,String user, String mail){
         return false;
     }
 }
---
+++ Validador/src/test/java/MiTest/TestJava.java
@@ -0,0 +1,25 @@
+package MiTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Validador.ControladorUsuario;
+
+import java.io.IOException;
+
+public class TestJava {
+    @Test
+    public void testValidador() {
+
+        String contrasenia = "fortune12";
+        try {
+            Assert.assertEquals(
+                    "Pone una mejor contraseña",
+                    "VALIDA",
+                    ControladorUsuario.validarConstrasenia(contrasenia, "", "").toString());
+        } catch (IOException e) {
+            System.out.println("Oops!");
+           // e.printStackTrace();
+        }
+    }
+}
