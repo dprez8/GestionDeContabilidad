8a7abf01adb6ad160d62be864161f431a2fcf582
FernandoHerbas <feer15.herbas@gmail.com>
2020-06-17 22:18:31 -0300

    Agrego un diagrama para los criterios y categorias de un egreso

--- Gestion de contabilidad/src/main/java/Organizacion/Empresa.java
+++ Gestion de contabilidad/src/main/java/Organizacion/Empresa.java
@@ -10,12 +10,12 @@ public class Empresa extends EntidadJuridica{
     private int cantidadDePersonal=2;  //valores hardcodeados temporalmente para testeo
     private double ventasAnuales=30000.0;  //valores hardcodeados temporalmente para testeo hay que crear setter
     private Categoria categoria;
-    private String rubro="Agropecuario";
+    private String sector="Agropecuario";
 
     public void cacularCategoria(List<Categoria> listaCategorias){
 //        int promedio = this.promedioDeVentasAnuales(); //Todavia no podemos desarrollar esta funcion, usamos atributo ventasAnuales temporalmente
 //        Categoria listaCategoriasOrdenado = listaCategorias.sort(); //Se podria sortear con un criterio propio, o suponer que se la pasa ordenada, o tomando los resultados ver cual es mayor.
-        List<Categoria> listaCategoriasFiltrada = listaCategorias.stream().filter(a -> a.getSector().equals(this.rubro))
+        List<Categoria> listaCategoriasFiltrada = listaCategorias.stream().filter(a -> a.getSector().equals(this.sector))
                 .filter(a -> a.dentroDelMinMax(this.ventasAnuales, this.cantidadDePersonal)).collect(Collectors.toList());
         Categoria categoriaObtenida = listaCategoriasFiltrada.stream().max(Comparator.comparingInt(Categoria::getCategoriaLevel)).get();
         this.categoria = categoriaObtenida;
