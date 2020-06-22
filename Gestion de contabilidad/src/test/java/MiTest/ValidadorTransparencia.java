89167b5b3efa1edf78cf51bb55e2aba1ca6837a1
FernandoHerbas <feer15.herbas@gmail.com>
2020-06-22 20:05:05 -0300

    Armo el test con sus respectivas instancias de una compra y presupuestos para una empresa

--- Gestion de contabilidad/src/test/java/MiTest/ValidadorTransparencia.java
+++ Gestion de contabilidad/src/test/java/MiTest/ValidadorTransparencia.java
@@ -1,9 +1,14 @@
 package MiTest;
 
+import DatosDeOperaciones.*;
 import Organizacion.*;
 import org.junit.Test;
 import ValidadorTransparencia.*;
+import Operaciones.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 
 public class ValidadorTransparencia {
@@ -15,6 +20,36 @@ public class ValidadorTransparencia {
 
     ValidadorDeTransparencia validador = new ValidadorDeTransparencia(validacionMenorValor,validacionMinima,validacionPresupuesto);
 
+    Producto RAM = new Producto("Memoria RAM 4 gb DDR3");
+    ItemEgreso RAMs = new ItemEgreso(RAM,1,3000);
+
+    Producto placaDeVideo = new Producto("4GB DDR5");
+    ItemEgreso placasDeVideo = new ItemEgreso(placaDeVideo,2,5000);
+
+    TipoDocumento FacturaA = new TipoDocumento("Factura A");
+    SimpleDateFormat parseador = new SimpleDateFormat("dd/MM/yy");
+    Date fechaDePedido = parseador.parse("31/01/20");
+    Date fechaDeEntrega = parseador.parse("05/02/20");
+    DocumentoComercial unDocumento = new DocumentoComercial(FacturaA,11111,fechaDePedido,fechaDeEntrega,"Ejemplo");
+
+    MedioDePago efectivo = new MedioDePago(1212,"Efectivo");
+
+    Proveedor lautaro = new Proveedor("Lautaro",41424242,1823);
+
+    Egreso unaCompra = new Egreso(unDocumento,efectivo,lautaro,miPyme,RAMs,placasDeVideo);
+
+    ItemPresupuesto RAMpresupuesto = new ItemPresupuesto(RAM,RAMs,1,3000);
+    ItemPresupuesto placaVideoPresupuesto = new ItemPresupuesto(placaDeVideo,placasDeVideo,2,5000);
+
+    ItemPresupuesto RAM2presupuesto = new ItemPresupuesto(RAM,RAMs,1,3500);
+    ItemPresupuesto placaVide2Presupuesto = new ItemPresupuesto(placaDeVideo,placasDeVideo,2,6000);
+
+    Presupuesto primerPresupuesto = new Presupuesto(unaCompra,unDocumento,"31/01/20",RAMpresupuesto,placaVideoPresupuesto);
+    Presupuesto segundoPresupuesto = new Presupuesto(unaCompra,unDocumento,"1/02/20",RAM2presupuesto,placaVide2Presupuesto);
+
+    public ValidadorTransparencia() throws ParseException {
+    }
+
     /**Quizas esta parte pueda ir en la clase Schudeler*/
     public void lanzarHilo(){
         Scheduler hilo = new Scheduler(miPyme,validador);
@@ -23,6 +58,8 @@ public class ValidadorTransparencia {
     }
     @Test
     public void correrHilo(){
+        unaCompra.agregarPresupuestos(primerPresupuesto,segundoPresupuesto);
+        miPyme.agregarOperaciones(unaCompra);
         lanzarHilo();
     }
 }
