9f09b0bd4723055bcf9936cdf38cb9ad304c6d25
FernandoHerbas <feer15.herbas@gmail.com>
2020-06-23 00:42:21 -0300

    Agrego un archivo de test para operaciones, separo test de validador de contrasenias en otro fichero

---
+++ Gestion de contabilidad/src/test/java/MiTest/TestValidadorContrasenia.java
@@ -0,0 +1,41 @@
+package MiTest;
+
+import CategorizadorDeEmpresas.Sector;
+import DatosDeOperaciones.*;
+import Exceptions.*;
+import Operaciones.*;
+import Organizacion.*;
+import Usuarios.*;
+import ValidadorDeContrasenia.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+public class TestValidadorContrasenia {
+
+    public TestValidadorContrasenia() throws ParseException {
+    }
+    @Test
+    public void testValidador() throws IOException, contraseniaCorta, contraseniaMuyComun, repiteContraseniaEnMailOUsuario {
+        Properties prop=new Properties();
+        FileInputStream ip= new FileInputStream("src/main/resources/config.properties");
+        //"src/main/resources/10k-worst-passwords.txt"
+        prop.load(ip);
+        Usuario.setNuevoControlUsuario(new ValidarLongitudCorta(Integer.parseInt(prop.getProperty("longitudMinima"))), new ValidarTop10k(prop.getProperty("dataFilePath")), new ValidarIgualAMailOUsuario());
+        //nose como hacer una config que devuelva objectos instanciados o que los instancie, creo que no puede
+
+        Sector servicios    = new Sector("Servicios");
+        Empresa CocaCola = new Empresa(12,130000,"SUMINISTRO DE AGUA",servicios);
+        Estandar fernando = new Estandar(CocaCola,"Fernando","fortune12","fer12@mail.com");
+        fernando.validarContraseña();
+        ip.close();
+    }
+}
