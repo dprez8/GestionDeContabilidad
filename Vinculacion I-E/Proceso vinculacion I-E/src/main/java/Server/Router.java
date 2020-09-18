fdb3e0e97f81a697b7119f16447cd99bdcc38e9b
FernandoHerbas <feer15.herbas@gmail.com>
2020-09-18 02:59:23 -0300

    Agrego dependencias de Spark y las clases necesarias para empezar la API

--- Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Server/Router.java
+++ Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Server/Router.java
@@ -1,4 +1,27 @@
 package Server;
 
+import Spark.utils.BooleanHelper;
+import Spark.utils.HandlebarsTemplateEngineBuilder;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class Router {
+    private static HandlebarsTemplateEngine engine;
+
+    private static void initEngine(){
+        Router.engine = HandlebarsTemplateEngineBuilder
+                .create()
+                .withDefaultHelpers()
+                .withHelper("isTrue", BooleanHelper.isTrue)
+                .build();
+    }
+    public static void init(){
+        Router.initEngine();
+        Spark.staticFileLocation("/public");
+        Router.configure();
+    }
+
+    private static void configure(){
+
+    }
 }
