966573310c659cafc5e8cb1c0d44819ed169366b
FernandoHerbas <feer15.herbas@gmail.com>
2020-09-18 02:59:23 -0300

    Agrego dependencias de Spark y las clases necesarias para empezar la API

--- Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Server/Server.java
+++ Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Server/Server.java
@@ -1,4 +1,13 @@
 package Server;
 
+import spark.Spark;
+import spark.debug.DebugScreen;
+
 public class Server {
+
+    public static void main() {
+        Spark.port(9001);
+        Router.init();
+        DebugScreen.enableDebugScreen();
+    }
 }
