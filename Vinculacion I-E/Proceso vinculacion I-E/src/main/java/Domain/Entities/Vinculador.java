3f7f3735f94d0af6d00e661b0f391c6a756c0154
FernandoHerbas <feer15.herbas@gmail.com>
2020-09-23 19:24:23 -0300

    Elimino la solucion anterior del diagrama
    Comienzo un test para el proceso de vinculacion, faltan detalles del codigo

--- Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Domain/Entities/Vinculador.java
+++ Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Domain/Entities/Vinculador.java
@@ -5,6 +5,7 @@ import Domain.Entities.Criterios.Criterio;
 import Domain.Entities.Operaciones.Egreso;
 import Domain.Entities.Operaciones.Ingreso;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Vinculador {
@@ -24,8 +25,41 @@ public class Vinculador {
 
             if(condiciones.stream().allMatch(unaCondicion->unaCondicion.cumpleCondicion(egreso, ingreso))) {
                 // Se puede vincular el egreso al ingreso
-                ingreso.addEgresosAsociados(egreso);
+                ingreso.addEgresoAsociado(egreso);
             }
         }
+    }
+
+    /*******Getters & Setters******************/
+    public List<Ingreso> getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(List<Ingreso> ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public List<Egreso> getEgresos() {
+        return egresos;
+    }
+
+    public void setEgresos(List<Egreso> egresos) {
+        this.egresos = egresos;
+    }
+
+    public List<Condicion> getCondiciones() {
+        return condiciones;
+    }
+
+    public void addCondiciones(Condicion ... condiciones) {
+        Collections.addAll(this.condiciones, condiciones);
+    }
+
+    public Criterio getCriterio() {
+        return criterio;
+    }
+
+    public void setCriterio(Criterio criterio) {
+        this.criterio = criterio;
     }
 }
---
+++ Vinculacion I-E/Proceso vinculacion I-E/src/test/java/TestPrincipal.java
@@ -0,0 +1,80 @@
+import Domain.Entities.Condiciones.CondicionEntreFechas;
+import Domain.Entities.Condiciones.CondicionValor;
+import Domain.Entities.Criterios.Criterio;
+import Domain.Entities.Criterios.CriterioUnico;
+import Domain.Entities.Criterios.OrdenValorPrimeroEgreso;
+import Domain.Entities.Operaciones.Egreso;
+import Domain.Entities.Operaciones.Ingreso;
+import Domain.Entities.Vinculador;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TestPrincipal {
+    private Vinculador vinculador;
+    private List<Ingreso> ingresos;
+    private List<Egreso> egresos;
+    private OrdenValorPrimeroEgreso ordenValorPrimeroEgreso;
+    private CondicionEntreFechas condicionEntreFechas;
+    private CondicionValor condicionValor;
+
+    @Before
+    public void antesDeTestear(){
+        /***********Creacion de las operaciones a asociar***************/
+        this.ingresos = new ArrayList<>();
+
+        Ingreso creditosHaberes = new Ingreso();
+        creditosHaberes.setMonto(10000.0);
+        creditosHaberes.setFecha(LocalDate.now());
+
+        Ingreso donacion = new Ingreso();
+        donacion.setMonto(500.0);
+        donacion.setFecha(LocalDate.now());
+
+        Collections.addAll(this.ingresos,creditosHaberes,donacion);
+
+        this.egresos = new ArrayList<>();
+
+        Egreso compraUno  = new Egreso();
+        compraUno.setFecha(LocalDate.now());
+        compraUno.setMonto(1000.0);
+
+        Egreso compraDos  = new Egreso();
+        compraDos.setFecha(LocalDate.now());
+        compraDos.setMonto(2500.0);
+
+        Egreso compraTres = new Egreso();
+        compraTres.setFecha(LocalDate.now());
+        compraTres.setMonto(10000.0);
+
+        Collections.addAll(this.egresos,compraDos,compraUno,compraTres);
+
+        /***************Creacion de condiciones*************/
+        this.condicionEntreFechas = new CondicionEntreFechas();
+        condicionEntreFechas.setDiasDesde(-3);
+        condicionEntreFechas.setDiasHasta(3);
+
+        this.condicionValor = new CondicionValor();
+
+        /***************Creacion criterio*******************/
+        this.ordenValorPrimeroEgreso = new OrdenValorPrimeroEgreso();
+
+        /***************Creacion vinculador*****************/
+        this.vinculador = new Vinculador();
+        this.vinculador.setIngresos(this.ingresos);
+        this.vinculador.setEgresos(this.egresos);
+        this.vinculador.addCondiciones(this.condicionValor,this.condicionEntreFechas);
+        this.vinculador.setCriterio(this.ordenValorPrimeroEgreso);
+
+    }
+
+    @Test
+    public void VinculacionExitosa(){
+
+    }
+}
