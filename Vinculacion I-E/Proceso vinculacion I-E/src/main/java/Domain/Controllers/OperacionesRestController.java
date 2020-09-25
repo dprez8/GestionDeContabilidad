07adadc170f505fb77adea9b59452b5a0eadb099
LautaroRobles <lautarorobles_oro@hotmail.com>
2020-09-25 19:29:43 -0300

    Preparando las clases de Spark

--- Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Domain/Controllers/OperacionesRestController.java
+++ Vinculacion I-E/Proceso vinculacion I-E/src/main/java/Domain/Controllers/OperacionesRestController.java
@@ -1,16 +1,24 @@
 package Domain.Controllers;
 
+import Domain.DTO.OperacionesDTO;
 import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 
 public class OperacionesRestController {
 
-    public Response obtenerOperaciones(Request request, Response response){
+    public Response obtenerOperaciones(Request request, Response response) {
+
+        Gson gson = new Gson();
+
+        OperacionesDTO operacionesDTO = gson.fromJson(request.body(), OperacionesDTO.class);
+
+        System.out.println(operacionesDTO.getEgresos());
+
         return response;
     }
 
-    public String retornarOperaciones(Request request, Response response){
+    public String retornarOperaciones(Request request, Response response) {
 
         Gson gson = new Gson();
 
