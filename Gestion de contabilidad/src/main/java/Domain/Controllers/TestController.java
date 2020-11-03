c2643d3663c40f0168ee3a090817025895d0b327
lait-utn <ulaitml@gmail.com>
2020-11-03 16:35:10 -0300

    Test andando de como subir un archivo
    Usando Jquery

--- Gestion de contabilidad/src/main/java/Domain/Controllers/TestController.java
+++ Gestion de contabilidad/src/main/java/Domain/Controllers/TestController.java
@@ -1,40 +1,71 @@
 package Domain.Controllers;
 
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Service;
 
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
 
 public class TestController {
 
-    public String mostrarFormularioCargarArchivo(Request request, Response response) {
-        return "<form method='post' enctype='multipart/form-data'>" // note the enctype
-                + "    <input type='file' name='uploaded_file' accept='.png'>" // make sure to call getPart using the same "name" in the post
-                + "    <button>Upload picture</button>"
-                + "</form>";
+    public ModelAndView mostrarFormularioCargarArchivo(Request request, Response response) {
+        return new ModelAndView(null, "carga-imagen.hbs");
     }
 
     public String cargarArchivo(Request request, Response response) throws IOException, ServletException {
 
-        File uploadDir = new File("upload");
+        String file_name = "doc_comercial_file";
+        File uploadDir = new File("src/main/resources/public/upload");
         uploadDir.mkdir(); // create the upload directory if it doesn't exist
 
-        Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
 
+        // configuracion standard
         request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
 
-        try (InputStream input = request.raw().getPart("uploaded_file").getInputStream()) { // getPart needs to use same "name" as input field in form
+        String uploaded_file_name = "";
+        // obtener el nombre del fichero para poder sacar la extension
+        // si falla es porque no habia archivo seguramente
+        try {
+            uploaded_file_name = getFileName(request.raw().getPart(file_name));
+        }
+        catch (NullPointerException exception) {
+            return "<h1>A null pointer exception occurred :C</h1>";
+        }
+        // creo el fichero temporal, con un nombre generico generado y la extension del archivo que fue subido
+        Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "." + getExtension(uploaded_file_name));
+        // copiar el stream de lo que me pasaron al fichero temporal
+        try (InputStream input = request.raw().getPart(file_name).getInputStream()) { // getPart needs to use same "name" as input field in form
             Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
         }
+        catch (NullPointerException exception) {
+            // fallo, borro el archivo temporal
+            Files.deleteIfExists(tempFile.toAbsolutePath());
+            return "<h1>A null pointer exception occurred :C</h1>";
+        }
 
-        return "<h1>You uploaded this image:<h1><img src='" + tempFile.getFileName() + ".png" + "'>";
+        System.out.println("Uploaded file '" + getFileName(request.raw().getPart(file_name)) + "' saved as '" + tempFile.toAbsolutePath() + "'");
+        return "<h1>You uploaded this image:</h1><img src='localhost/upload/" + tempFile.getFileName() + "'>";
+    }
+
+    private static String getFileName(Part part) {
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
     }
 }
--- Gestion de contabilidad/src/main/java/Server/Router.java
+++ Gestion de contabilidad/src/main/java/Server/Router.java
@@ -48,7 +48,10 @@ public class Router {
     private static void rutasVista() {
         LoginController loginController = new LoginController();
         PanelController panelController = new PanelController();
-        
+        TestController testController = new TestController();
+
+        Spark.get("/test/cargar-archivo", testController::mostrarFormularioCargarArchivo, Router.engine);
+        Spark.post("/test/cargar-archivo", testController::cargarArchivo);
         Spark.get("/login", loginController::mostrarLogin, Router.engine);
         Spark.get("/*", panelController::mostrarInicio, Router.engine);
     }
@@ -63,7 +66,6 @@ public class Router {
         CategoriasEgresosController categoriasController = new CategoriasEgresosController();
         IngresosRestController ingresosRestController = new IngresosRestController();
         AsociacionOperacionesRestController asociacionOperacionesRestController = new AsociacionOperacionesRestController();
-        TestController testController = new TestController();
 
         Spark.post("/api/login",loginRestController::login);
         Spark.get("/api/login",loginRestController::sessionStatus);
@@ -85,8 +87,6 @@ public class Router {
         Spark.post("/api/operaciones/asociarManualmente",asociacionOperacionesRestController::asociarManualmente);
         Spark.post("/api/categorias/asociar",categoriasController::asociarCategoriaEgreso);
         Spark.post("/api/operaciones/ingreso",ingresosRestController::cargarNuevoIngreso);
-        Spark.get("/api/test/cargar-archivo", testController::mostrarFormularioCargarArchivo);
-        Spark.post("/api/test/cargar-archivo", testController::cargarArchivo);
     }
 
     private static void verificarTareasProgramadas() {
